7475beebe59e6e73e79c679e32b4c4de63366c54
csyun <changsoonyun@naver.com>
2024-01-26 21:01:39 +0900

    Fix main컴포넌트 App컴포넌트 렌더링 하게 수정

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
 import ReactDOM from 'react-dom/client';
 import '../app/globals.css';
 import { BrowserRouter } from 'react-router-dom';
-import Router from '@/lib/router/Router.tsx';
+import App from '@/App.tsx';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <BrowserRouter>
-    <Router />
+    <App />
   </BrowserRouter>
 );
