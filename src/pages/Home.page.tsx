42ce702fe2cae51b3fc97fb09918f90ea8a5571f
csyun <changsoonyun@naver.com>
2024-01-27 03:04:37 +0900

    #7 로그인 로그아웃 로직 페이지 적용

--- src/pages/Home.page.tsx
+++ src/pages/Home.page.tsx
@@ -1,3 +1,7 @@
+import { useAuth } from '@/apis/useAuth.ts';
+
 export default function HomePage() {
-  return <>Home</>;
+  const { isLoggedIn } = useAuth();
+
+  return <>Home {isLoggedIn ? '로그인함' : '아님'}</>;
 }
