28d58add689b5db93ca6bfad6b1a5ed6384b897d
csyun <changsoonyun@naver.com>
2024-01-27 03:03:37 +0900

    #7 QueryClientProvider, Devtools App에 적용

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,15 @@
 import Router from '@/lib/router/Router.tsx';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
+export const queryClient = new QueryClient();
 function App() {
   return (
     <>
-      <Router />
+      <QueryClientProvider client={queryClient}>
+        <Router />
+        <ReactQueryDevtools />
+      </QueryClientProvider>
     </>
   );
 }
