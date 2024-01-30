4082bbaa2c36918ca21f65a5e03c34e8f202051a
csyun <changsoonyun@naver.com>
2024-01-30 21:05:33 +0900

    Fix 콘솔로그 제거

--- src/lib/router/PrivateRouter.tsx
+++ src/lib/router/PrivateRouter.tsx
@@ -3,6 +3,5 @@ import { useAuth } from '@/apis/useAuth.ts';
 
 export default function PrivateRouter() {
   const { isLoggedIn } = useAuth();
-  console.log('isLoggedIn', isLoggedIn);
   return <>{isLoggedIn ? <Outlet /> : <Navigate to={'/'} />}</>;
 }
