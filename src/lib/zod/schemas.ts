be2c0ca95554e093dc9924bef9b994ad5c656a37
csyun <changsoonyun@naver.com>
2024-01-30 21:06:10 +0900

    Add productAddFormSchema 추가

--- src/lib/zod/schemas.ts
+++ src/lib/zod/schemas.ts
@@ -13,3 +13,9 @@ export const registerFormSchema = loginFormSchema.extend({
     .min(1, { message: '이름은 최소 1자 이상 입니다.' })
     .max(20, { message: '이름은 최대 20자 입니다.' }),
 });
+
+export const productAddFormSchema = z.object({
+  title: z.string().min(1, { message: '제목은 최소 1글자 이상 입니다.' }),
+  // password: z.string().regex(regex, { message: '비밀번호 형식에 맞지 않습니다.' }),
+  desc: z.string().min(1, { message: '내용은 최소 1글자 이상 입니다.' }),
+});
