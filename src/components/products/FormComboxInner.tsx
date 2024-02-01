32b51aaebe0aa900553b10fc050c21acb1fe1edb
csyun <changsoonyun@naver.com>
2024-02-02 00:14:11 +0900

    #19 Feat Combobox, RadioGroup 컴포넌트화

---
+++ src/components/products/FormComboxInner.tsx
@@ -0,0 +1,88 @@
+import { FieldValues, Path, UseFormReturn } from 'react-hook-form';
+import {
+  Command,
+  CommandEmpty,
+  CommandGroup,
+  CommandInput,
+  CommandItem,
+} from '@/components/ui/command';
+import { FormControl, FormField, FormItem, FormLabel, FormMessage } from '@/components/ui/form';
+import { Popover, PopoverContent, PopoverTrigger } from '@/components/ui/popover';
+import { categories } from '@/constant/categories.ts';
+import { Check, ChevronsUpDown } from 'lucide-react';
+import { cn } from '@/lib/utils.ts';
+import { Button } from '@/components/ui/button.tsx';
+import { useState } from 'react';
+
+interface FormInnerProps<T extends FieldValues> {
+  form: UseFormReturn<T>;
+  name: Path<T>;
+  label: string;
+}
+
+export default function FormComboxInner<T extends FieldValues>({
+  form,
+  name,
+  label,
+}: FormInnerProps<T>) {
+  const [open, setOpen] = useState(false);
+  return (
+    <>
+      <div className={'pb-4'}>
+        <FormField
+          control={form.control}
+          name={name}
+          render={({ field }) => (
+            <FormItem className="flex flex-col">
+              <FormLabel style={{ color: 'black' }}>{label}</FormLabel>
+              <Popover open={open} onOpenChange={setOpen}>
+                <PopoverTrigger asChild>
+                  <FormControl>
+                    <Button
+                      variant="outline"
+                      role="combobox"
+                      className={cn(
+                        'w-[200px] justify-between',
+                        !field.value && 'text-muted-foreground'
+                      )}>
+                      {field.value
+                        ? categories.find((category) => category.value === field.value)?.label
+                        : '카테고리 선택'}
+                      <ChevronsUpDown className="ml-2 h-4 w-4 shrink-0 opacity-50" />
+                    </Button>
+                  </FormControl>
+                </PopoverTrigger>
+                <PopoverContent className="w-[200px] p-0">
+                  <Command>
+                    <CommandInput placeholder="카테고리 검색" />
+                    <CommandEmpty>검색된 카테고리가 없습니다.</CommandEmpty>
+                    <CommandGroup>
+                      {categories.map((category) => (
+                        <CommandItem
+                          value={category.label}
+                          key={category.value}
+                          onSelect={() => {
+                            // @ts-ignore
+                            form.setValue(name, category.value);
+                          }}>
+                          <Check
+                            className={cn(
+                              'mr-2 h-4 w-4',
+                              category.value === field.value ? 'opacity-100' : 'opacity-0'
+                            )}
+                          />
+                          {category.label}
+                        </CommandItem>
+                      ))}
+                    </CommandGroup>
+                  </Command>
+                </PopoverContent>
+              </Popover>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+      </div>
+    </>
+  );
+}
---
+++ src/components/products/FormRadioGroup.tsx
@@ -0,0 +1,52 @@
+'use client';
+
+import { FieldValues, Path, UseFormReturn } from 'react-hook-form';
+
+import { FormControl, FormField, FormItem, FormLabel, FormMessage } from '@/components/ui/form';
+import { RadioGroup, RadioGroupItem } from '@/components/ui/radio-group';
+import { conditions } from '@/constant/conditions.ts';
+
+interface FormInnerProps<T extends FieldValues> {
+  form: UseFormReturn<T>;
+  name: Path<T>;
+  label: string;
+}
+export default function FormRadioGroup<T extends FieldValues>({
+  form,
+  name,
+  label,
+}: FormInnerProps<T>) {
+  return (
+    <>
+      <div className="pb-4">
+        <FormField
+          control={form.control}
+          name={name}
+          render={({ field }) => (
+            <FormItem className="space-y-3">
+              <FormLabel className={'text-black'}>{label}</FormLabel>
+              <FormControl>
+                <RadioGroup
+                  onValueChange={field.onChange}
+                  value={field.value}
+                  className="flex flex-wrap gap-4">
+                  {conditions.map((condition) => (
+                    <FormItem
+                      key={condition.value}
+                      className="flex items-center space-x-1 space-y-0">
+                      <FormControl>
+                        <RadioGroupItem value={condition.value} />
+                      </FormControl>
+                      <FormLabel className="font-normal">{condition.label}</FormLabel>
+                    </FormItem>
+                  ))}
+                </RadioGroup>
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+      </div>
+    </>
+  );
+}
