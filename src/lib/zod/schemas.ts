ce515d43868bd40c4bcfa46a5b5f229d5a89fd00
csyun <changsoonyun@naver.com>
2024-02-02 00:13:54 +0900

    #19 Feat product 추가정보 관련 스키마 추가

--- src/lib/zod/schemas.ts
+++ src/lib/zod/schemas.ts
@@ -14,9 +14,12 @@ export const registerFormSchema = loginFormSchema.extend({
     .max(20, { message: '이름은 최대 20자 입니다.' }),
 });
 
-export const productAddFormSchema = z.object({
+export const productFormSchema = z.object({
   title: z.string().min(1, { message: '제목은 최소 1글자 이상 입니다.' }),
   desc: z.string().min(1, { message: '내용은 최소 1글자 이상 입니다.' }),
+  category: z.string().min(1, { message: '카테고리를 선택해 주세요.' }),
+  price: z.string().min(1, { message: '가격은 100원 이상입니다.' }).or(z.number()),
+  condition: z.string().min(1, { message: '상태를 선택해주세요.' }),
   imgList: z
     .array(
       z.object({
