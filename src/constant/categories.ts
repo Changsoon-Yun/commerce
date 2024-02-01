e0182e8b6e473785c3088bbd298917d6dc7aa740
csyun <changsoonyun@naver.com>
2024-02-02 00:13:31 +0900

    #19 Add constant dir

---
+++ src/constant/categories.ts
@@ -0,0 +1,13 @@
+export const categories = [
+  { label: '의류 및 패션', value: 'clothing_fashion' },
+  { label: '가전제품', value: 'electronics_appliances' },
+  { label: '가구 및 생활용품', value: 'furniture_home_goods' },
+  { label: '도서 및 음반', value: 'books_music' },
+  { label: '스포츠 및 레저', value: 'sports_outdoors' },
+  { label: '자동차 및 오토바이', value: 'cars_motorcycles' },
+  { label: '아동용품', value: 'kids_baby_items' },
+  { label: '뷰티 및 건강', value: 'beauty_health' },
+  { label: '취미 및 공예', value: 'hobbies_crafts' },
+  { label: '티켓 및 이벤트', value: 'tickets_events' },
+  { label: '기타', value: 'etc' },
+] as const;
---
+++ src/constant/conditions.ts
@@ -0,0 +1,9 @@
+export const conditions = [
+  { label: '미개봉', value: 'unopened' },
+  { label: '최상급', value: 'like_new' },
+  { label: '상급', value: 'excellent' },
+  { label: '중상급', value: 'good' },
+  { label: '하급', value: 'fair' },
+  { label: '파손', value: 'damaged' },
+  { label: '수리 필요', value: 'needs_repair' },
+] as const;
