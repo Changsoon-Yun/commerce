5b0b3791813ad5157cb689c9bcc71291d44987c2
csyun <changsoonyun@naver.com>
2024-02-02 06:24:59 +0900

    #21 Fix useQuery에 disabled 추가

--- src/apis/useGetSellerProduct.ts
+++ src/apis/useGetSellerProduct.ts
@@ -7,15 +7,17 @@ export default function useGetSellerProduct({ id }: { id: string }) {
   const { storedUserData } = useAuth();
 
   const fetchData = async () => {
-    const q = doc(db, `products/${storedUserData?.uid}/products`, id);
+    const q = doc(db, `products/${id}`);
     const querySnapshot = await getDoc(q);
 
     return querySnapshot.data();
   };
 
   const { data: product } = useQuery({
-    queryKey: [`products/${storedUserData?.uid}/${id}`],
+    queryKey: [`products`, storedUserData?.uid, id],
     queryFn: fetchData,
+    // id가 있을때만 fetching
+    enabled: !!id,
   });
   return { product };
 }
