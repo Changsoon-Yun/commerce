4b23f205601629f064d4d45447968a8a948e4109
csyun <changsoonyun@naver.com>
2024-02-03 11:38:11 +0900

    #16 Feat 메인 카테고리 추가 및 소셜버튼 추가

--- src/components/products/home/CardWithCategory.tsx
+++ src/components/products/home/CardWithCategory.tsx
@@ -1,38 +1,40 @@
 import { Link } from 'react-router-dom';
-import { Card, CardContent } from '@/components/ui/card.tsx';
+import { Card, CardContent, CardFooter } from '@/components/ui/card.tsx';
 import useGetProducts from '@/apis/useGetProducts';
 import { Carousel, CarouselContent, CarouselItem } from '@/components/ui/carousel';
 import { formatNumberWithCommas } from '@/utils/converter.ts';
+import { Button } from '@/components/ui/button.tsx';
+import { Separator } from '@/components/ui/separator.tsx';
 
 interface CardWithCategoryProps {
   category: { label: string; value: string };
 }
 export default function CardWithCategory({ category }: CardWithCategoryProps) {
   const { products } = useGetProducts(category.value);
-  console.log(products);
   return (
     <>
       <div key={category.value}>
-        <div className={'pb-8'}>
-          <div className={'flex justify-between items-center'}>
-            <h3 className="scroll-m-20 text-2xl font-semibold tracking-tight pb-4">
-              {category.label}
+        <div className={'pb-20'}>
+          <div className={'flex flex-col justify-center items-center pb-8 gap-4'}>
+            <h3 className="scroll-m-20 text-xl font-semibold tracking-tight">
+              <Link to={`products/${category.value}`}>
+                <div className={'pb-2'}>{category.label}</div>
+              </Link>
+              <Separator className={'h-0.5 bg-zinc-300'} />
             </h3>
-            <small className="text-sm font-medium leading-none text-zinc-400">
-              <Link to={`products/${category.value}`}>더보기</Link>
-            </small>
           </div>
+
           <div className={'flex gap-2'}>
-            {products?.map(({ title, imageList, price, createdAt }) => (
-              <Card className="flex-1 max-w-[240px] overflow-hidden" key={createdAt.seconds}>
+            {products?.map(({ title, imageList, id, price }) => (
+              <Card className="flex-1 max-w-[240px] overflow-hidden" key={id}>
                 <div>
                   <Carousel className="w-full max-w-xs" opts={{ active: imageList.length > 1 }}>
-                    <CarouselContent className={''}>
+                    <CarouselContent>
                       {imageList.map((src, index) => (
                         <CarouselItem key={index} className={'border-0'}>
                           <div className={'relative'}>
                             {imageList.length == 1 && (
-                              <div className={'absolute t-0 l-0 w-full h-full'}></div>
+                              <div className={'layer absolute t-0 l-0 w-full h-full'}></div>
                             )}
                             <Card className={'p-2 shadow-none border-0'}>
                               <CardContent className="flex aspect-square items-center justify-center p-0">
@@ -59,6 +61,11 @@ export default function CardWithCategory({ category }: CardWithCategoryProps) {
                     {formatNumberWithCommas(price)}원
                   </div>
                 </CardContent>
+                <CardFooter className={'p-2'}>
+                  <Button variant={'secondary'} className={'w-full'} asChild>
+                    <Link to={`/product/${id}`}>상세보기</Link>
+                  </Button>
+                </CardFooter>
               </Card>
             ))}
           </div>
