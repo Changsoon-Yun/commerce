e670cd0f261883bba00a5ba731c1947726efdb70
csyun <changsoonyun@naver.com>
2024-02-04 05:45:21 +0900

    Feat Footer.tsx layout에 추가

--- src/components/ui/CommonLayout.tsx
+++ src/components/ui/CommonLayout.tsx
@@ -1,5 +1,6 @@
 import { Outlet } from 'react-router-dom';
 import Header from './Header';
+import Footer from '@/components/ui/Footer.tsx';
 
 export default function CommonLayout() {
   return (
@@ -12,6 +13,11 @@ export default function CommonLayout() {
       <div className={'max-w-6xl m-auto'}>
         <div className={'m-5'}>
           <Outlet />
+        </div>
+      </div>
+      <div className={'border-t border-zinc-200 bg-zinc-50'}>
+        <div className={'max-w-6xl m-auto'}>
+          <Footer />
         </div>
       </div>
     </>
