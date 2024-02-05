03f8bcb938c0bfd3efaed2028e97135036854221
csyun <changsoonyun@naver.com>
2024-02-05 23:28:53 +0900

    #25 Feat 장바구니 조회 컴포넌트 수정 및 로직 추가

---
+++ src/components/cart/CartList.tsx
@@ -0,0 +1,68 @@
+import useGetProduct from '@/apis/useGetProduct.ts';
+import { Button } from '@/components/ui/button.tsx';
+import { useContext } from 'react';
+import { CartContext } from '@/context/CartContext.tsx';
+import { Link } from 'react-router-dom';
+import { formatNumberWithCommas } from '@/utils/converter';
+import { Carousel, CarouselContent, CarouselItem } from '@/components/ui/carousel.tsx';
+import { Card, CardContent, CardFooter } from '@/components/ui/card.tsx';
+
+export default function CartList({ id }: { id: string }) {
+  const { product } = useGetProduct({ id });
+  const { removeCart, toggleHandler } = useContext(CartContext);
+  if (!product) {
+    return;
+  }
+  const { imageList, title, price } = product;
+  return (
+    <>
+      <Card className="w-full" key={id}>
+        <Carousel className="w-full max-w-xs" opts={{ active: imageList.length > 1 }}>
+          <CarouselContent>
+            {imageList.map((src, index) => (
+              <CarouselItem key={index} className={'border-0'}>
+                <div className={'relative'}>
+                  {imageList.length == 1 && (
+                    <div className={'layer absolute t-0 l-0 w-full h-full'}></div>
+                  )}
+                  <Card className={'p-2 shadow-none border-0'}>
+                    <CardContent className="flex aspect-square items-center justify-center p-0">
+                      <div className={'w-full h-full rounded-lg overflow-hidden'}>
+                        <img src={src} alt="img" className={'w-full h-full object-cover'} />
+                      </div>
+                    </CardContent>
+                  </Card>
+                </div>
+              </CarouselItem>
+            ))}
+          </CarouselContent>
+        </Carousel>
+        <CardContent className={'p-2'}>
+          <h3 className="scroll-m-20 text-lg tracking-tight line-clamp-2 min-h-[56px] pb-1">
+            {title}
+          </h3>
+          <div className="text-sm font-semibold line-clamp-2">
+            {formatNumberWithCommas(price)}원
+          </div>
+        </CardContent>
+        <CardFooter className={'p-2'}>
+          <Button
+            variant={'secondary'}
+            className={'w-full'}
+            asChild
+            onClick={() => {
+              toggleHandler();
+            }}>
+            <Link to={`/product/${id}`}>상세보기</Link>
+          </Button>
+          <Button
+            onClick={() => {
+              removeCart(id);
+            }}>
+            삭제하기
+          </Button>
+        </CardFooter>
+      </Card>
+    </>
+  );
+}
---
+++ src/components/cart/SideCart.tsx
@@ -0,0 +1,34 @@
+import { useContext } from 'react';
+import { CartContext } from '@/context/CartContext.tsx';
+import { Button } from '@/components/ui/button.tsx';
+import CartList from './CartList';
+
+export default function SideCart() {
+  const { isOpen, carts, toggleHandler } = useContext(CartContext);
+
+  return (
+    <>
+      <div
+        style={{ height: `calc(100vh - 80px)`, transition: 'right 0.3s ease-in-out' }}
+        className={`fixed top-[80px] w-[300px] px-4 border-l border-zinc-200  bg-zinc-50 overflow-auto ${isOpen ? 'right-[-300px]' : 'right-[0px]'}`}>
+        <div className={'relative'}>
+          <Button
+            onClick={() => {
+              toggleHandler();
+            }}>
+            닫기
+          </Button>
+          <h2 className="scroll-m-20 py-10 text-3xl font-semibold tracking-tight first:mt-0 text-center">
+            찜한 목록
+          </h2>
+          <div className={'flex gap-2 flex-wrap'}>
+            {carts.map((id) => (
+              <CartList id={id} key={id} />
+            ))}
+          </div>
+          <div>총 가격 : 어떻게 암</div>
+        </div>
+      </div>
+    </>
+  );
+}
