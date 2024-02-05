18a00269d2d95257445f7b5ec7277c82c41fe4e1
csyun <changsoonyun@naver.com>
2024-02-05 23:29:20 +0900

    Style 헤더 컴포넌트를 sticky로 변경

--- src/components/ui/CommonLayout.tsx
+++ src/components/ui/CommonLayout.tsx
@@ -1,24 +1,28 @@
 import { Outlet } from 'react-router-dom';
 import Header from './Header';
 import Footer from '@/components/ui/Footer.tsx';
+import SideCart from '@/components/cart/SideCart.tsx';
 
 export default function CommonLayout() {
   return (
     <>
-      <div className={'border-b border-zinc-200 bg-zinc-50'}>
-        <div className={'max-w-6xl m-auto'}>
-          <Header />
+      <div className={'relative'}>
+        <div className={'sticky top-0 z-10 border-b border-zinc-200 bg-zinc-50'}>
+          <div className={'max-w-6xl m-auto'}>
+            <Header />
+          </div>
         </div>
-      </div>
-      <div className={'max-w-6xl m-auto min-h-screen'}>
-        <div className={'m-5'}>
-          <Outlet />
+        <div className={'max-w-6xl m-auto min-h-screen'}>
+          <div className={'m-5'}>
+            <Outlet />
+          </div>
         </div>
-      </div>
-      <div className={'border-t border-zinc-200 bg-zinc-50'}>
-        <div className={'max-w-6xl m-auto'}>
-          <Footer />
+        <div className={'border-t border-zinc-200 bg-zinc-50'}>
+          <div className={'max-w-6xl m-auto'}>
+            <Footer />
+          </div>
         </div>
+        <SideCart />
       </div>
     </>
   );
