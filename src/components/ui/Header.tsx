4ca14bebfeedd0fef3c56c6d9d17b0453bb08259
csyun <changsoonyun@naver.com>
2024-02-05 17:39:46 +0900

    #25 Feat 헤더에 찜한 갯수 표현

--- src/components/ui/Header.tsx
+++ src/components/ui/Header.tsx
@@ -12,9 +12,14 @@ import { NavigationMenuItem, navigationMenuTriggerStyle } from './navigation-men
 import { Link } from 'react-router-dom';
 import { useAuth } from '@/apis/useAuth.ts';
 import { Button } from './button';
+import { useContext } from 'react';
+import { CartContext } from '@/context/CartContext.tsx';
+import { FaRegHeart } from 'react-icons/fa';
 
 export default function Header() {
   const { storedUserData, logout } = useAuth();
+  const { isOpen, openHandler, closeHandler } = useContext(CartContext);
+  const { carts } = useContext(CartContext);
   return (
     <>
       <div className={'flex justify-between px-5 items-center'}>
@@ -24,29 +29,48 @@ export default function Header() {
         <Input className={'max-w-md'} type="text" placeholder="검색" />
         <div className={'flex gap-4'}>
           {storedUserData ? (
-            <DropdownMenu>
-              <DropdownMenuTrigger className={'focus-visible:outline-none'}>
-                <Avatar>
-                  <AvatarImage src="/img/defaultProfileImage.png" />
-                </Avatar>
-              </DropdownMenuTrigger>
-              <DropdownMenuContent>
-                <DropdownMenuItem>
-                  <Link to={'/user/dashboard'}>내 프로필 보기</Link>
-                </DropdownMenuItem>
-                <DropdownMenuSeparator />
-                {storedUserData?.isSeller && (
-                  <>
-                    <DropdownMenuLabel>판매자 메뉴</DropdownMenuLabel>
-                    <Link to={'/seller/dashboard'}>
-                      <DropdownMenuItem>판매 목록</DropdownMenuItem>
-                    </Link>
-                    <DropdownMenuSeparator />
-                  </>
+            <div className={'flex items-center gap-4'}>
+              <div
+                onClick={() => {
+                  !isOpen ? openHandler() : closeHandler();
+                }}
+                className={
+                  'relative p-4 cursor-pointer text-zinc-500 hover:text-zinc-600 transition'
+                }>
+                <FaRegHeart size={24} />
+                {carts.length > 0 && (
+                  <div
+                    className={
+                      'absolute top-0 right-0 rounded-full w-[24px] h-[24px] flex items-center justify-center bg-[#f6d0cb] text-sm'
+                    }>
+                    {carts.length}
+                  </div>
                 )}
-                <DropdownMenuItem onClick={() => logout()}>로그아웃</DropdownMenuItem>
-              </DropdownMenuContent>
-            </DropdownMenu>
+              </div>
+              <DropdownMenu>
+                <DropdownMenuTrigger className={'focus-visible:outline-none'}>
+                  <Avatar>
+                    <AvatarImage src="/img/defaultProfileImage.png" />
+                  </Avatar>
+                </DropdownMenuTrigger>
+                <DropdownMenuContent>
+                  <DropdownMenuItem>
+                    <Link to={'/user/dashboard'}>내 프로필 보기</Link>
+                  </DropdownMenuItem>
+                  <DropdownMenuSeparator />
+                  {storedUserData?.isSeller && (
+                    <>
+                      <DropdownMenuLabel>판매자 메뉴</DropdownMenuLabel>
+                      <Link to={'/seller/dashboard'}>
+                        <DropdownMenuItem>판매 목록</DropdownMenuItem>
+                      </Link>
+                      <DropdownMenuSeparator />
+                    </>
+                  )}
+                  <DropdownMenuItem onClick={() => logout()}>로그아웃</DropdownMenuItem>
+                </DropdownMenuContent>
+              </DropdownMenu>
+            </div>
           ) : (
             <NavigationMenuItem className={navigationMenuTriggerStyle() + ' px-0'}>
               <Link to={'/login'}>
