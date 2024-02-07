d2ef69ba8e1a7c23dd932e551be1d77d0273b4e8
csyun <changsoonyun@naver.com>
2024-02-07 12:37:13 +0900

    #34 Feat 리액트 쿼리키 관련 객체 제작

---
+++ src/lib/react-query/queryKeys.ts
@@ -0,0 +1,20 @@
+import { FilterOptions } from '@/apis/useGetCategoryProducts.tsx';
+
+export const QUERY_KEYS = {
+  AUTH: {
+    BASE: ['userInfo'] as const,
+    USER: () => [QUERY_KEYS.AUTH.BASE[0]] as const,
+  },
+  PRODUCTS: {
+    BASE: ['products'] as const,
+    HOME: (category: string) => [QUERY_KEYS.PRODUCTS.BASE[0], 'home', category] as const,
+    SELLER: (uid: string) => [QUERY_KEYS.PRODUCTS.BASE[0], uid] as const,
+    CATEGORY: (category: string, filter: FilterOptions) =>
+      [QUERY_KEYS.PRODUCTS.BASE[0], category, filter] as const,
+  },
+  PRODUCT: {
+    BASE: ['product'] as const,
+    SELLER: (uid: string, id: string) => [QUERY_KEYS.PRODUCT.BASE[0], uid, id] as const,
+    DETAIL: (id: string) => [QUERY_KEYS.PRODUCT.BASE[0], id],
+  },
+};
