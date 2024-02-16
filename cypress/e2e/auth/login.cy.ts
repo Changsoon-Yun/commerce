72592ba5cab3614893f5e8c912837a3d1e25463c
csyun <changsoonyun@naver.com>
2024-02-17 03:00:56 +0900

    #46 Test 정상 로그인, 잘못된 인풋 입력으로 인한 에러 메세지 출력

---
+++ cypress/e2e/auth/login.cy.ts
@@ -0,0 +1,46 @@
+import { user } from '../../fixtures/user.ts';
+
+describe('로그인 테스트', () => {
+  const { email, password } = user;
+
+  beforeEach(() => {
+    cy.visit('/login');
+    cy.get('[data-cy="heading-text"]').should('have.text', '로그인');
+
+    cy.get('input[name="email"]').as('email-input').should('exist');
+    cy.get('input[name="password"]').as('password-input').should('exist');
+    cy.get('[data-cy="login-button"]').as('login-button').should('exist');
+  });
+  it('정상적인 로그인 시도', () => {
+    cy.get('@email-input').type(email);
+    cy.get('@email-input').invoke('val').should('eq', email);
+
+    cy.get('@password-input').type(password);
+    cy.get('@password-input').invoke('val').should('eq', password);
+
+    cy.get('@login-button').click();
+    cy.url().should('eq', 'http://localhost:5173/');
+  });
+
+  context('잘못된 인풋 입력', () => {
+    it('이메일 에러 메세지 출력', () => {
+      cy.get('@email-input').type('a');
+      cy.get('@login-button').click();
+      cy.get('[data-cy="email-error-msg"]').should('have.text', '이메일 형식에 맞지 않습니다.');
+    });
+
+    it('비밀번호 에러 메세지 출력', () => {
+      cy.get('@email-input').type(email);
+      cy.get('[data-cy="email-error-msg"]').should('not.exist');
+
+      cy.get('@password-input').type('1q');
+      cy.get('@login-button').click();
+      cy.get('[data-cy="password-error-msg"]').should(
+        'have.text',
+        '비밀번호 형식에 맞지 않습니다.'
+      );
+    });
+  });
+
+  afterEach(() => {});
+});
