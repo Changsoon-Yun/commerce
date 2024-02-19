49c200361bb19cc9d3fa287e5c73c34fe3d12412
csyun <changsoonyun@naver.com>
2024-02-20 00:38:20 +0900

    #49 Feat fetchData에 useCallback 및 getNextPageParam의 return값 간소화

--- src/apis/useGetCategoryProducts.ts
+++ src/apis/useGetCategoryProducts.ts
@@ -12,7 +12,7 @@ import {
 } from 'firebase/firestore';
 import { useInfiniteQuery } from '@tanstack/react-query';
 import { useInView } from 'react-intersection-observer';
-import { useEffect, useMemo } from 'react';
+import { useCallback, useEffect, useMemo } from 'react';
 import { QUERY_KEYS } from '@/lib/react-query/queryKeys.ts';
 
 export interface FilterOptions {
@@ -40,33 +40,39 @@ interface Product {
   };
 }
 export default function useGetCategoryProducts({ category, filter }: Options) {
-  const fetchData = async (
-    lastVisible: QueryDocumentSnapshot<DocumentData, DocumentData> | undefined
-  ) => {
-    const q = !lastVisible
-      ? query(
-          collection(db, `products`),
-          where('category', '==', category),
-          orderBy(filter.option, filter.direction),
-          limit(8)
-        )
-      : query(
-          collection(db, `products`),
-          where('category', '==', category),
-          orderBy(filter.option, filter.direction),
-          startAfter(lastVisible),
-          limit(8)
-        );
-    const querySnapshot = await getDocs(q);
+  const fetchData = useCallback(
+    async (lastVisible: QueryDocumentSnapshot<DocumentData, DocumentData> | undefined) => {
+      const q = !lastVisible
+        ? query(
+            collection(db, `products`),
+            where('category', '==', category),
+            orderBy(filter.option, filter.direction),
+            limit(8)
+          )
+        : query(
+            collection(db, `products`),
+            where('category', '==', category),
+            orderBy(filter.option, filter.direction),
+            startAfter(lastVisible),
+            limit(8)
+          );
+      const querySnapshot = await getDocs(q);
 
-    const products: Product[] = [];
+      const products: Product[] = [];
 
-    querySnapshot.forEach((doc) => {
-      products.push({ id: doc.id, ...doc.data() } as Product);
-    });
+      querySnapshot.forEach((doc) => {
+        products.push({ id: doc.id, ...doc.data() } as Product);
+      });
 
-    return { products, querySnapshot };
-  };
+      return { products, querySnapshot };
+    },
+    [category, filter]
+  );
+
+  const queryKey = useMemo(
+    () => QUERY_KEYS.PRODUCTS.CATEGORY(category, filter),
+    [category, filter]
+  );
 
   const {
     data: products,
@@ -74,7 +80,7 @@ export default function useGetCategoryProducts({ category, filter }: Options) {
     hasNextPage,
     isFetchingNextPage,
   } = useInfiniteQuery({
-    queryKey: useMemo(() => QUERY_KEYS.PRODUCTS.CATEGORY(category, filter), [category, filter]),
+    queryKey,
     initialPageParam: undefined,
     queryFn: ({
       pageParam,
@@ -84,11 +90,7 @@ export default function useGetCategoryProducts({ category, filter }: Options) {
     enabled: !!category,
     getNextPageParam: (lastPage) => {
       const lastVisible = lastPage.querySnapshot.docs[lastPage.querySnapshot.docs.length - 1];
-      if (lastPage.querySnapshot.size < 8) {
-        return undefined;
-      } else {
-        return lastVisible;
-      }
+      return lastPage.querySnapshot.size >= 8 ? lastVisible : undefined;
     },
   });
 
