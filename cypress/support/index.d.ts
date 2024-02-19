a1fc019d5fc0d5d9d3a3accee7808f94fedae33f
csyun <changsoonyun@naver.com>
2024-02-19 16:16:57 +0900

    #53 Test 디테일 페이지 테스트를 위해 cypress 커스텀 커맨드 추가

--- cypress/support/index.d.ts
+++ cypress/support/index.d.ts
@@ -2,5 +2,10 @@ declare namespace Cypress {
   interface Chainable {
     signInWithEmailAndPassword(type: 'seller' | 'customer'): void;
     signOut(): void;
+    sortAndVerify(
+      sortOption: '최신순' | '오래된순' | '높은 가격순' | '낮은 가격순',
+      sortingCriteria: 'product-price' | 'product-date',
+      compareFunction: ((a: string, b: string) => number) | undefined
+    ): void;
   }
 }
