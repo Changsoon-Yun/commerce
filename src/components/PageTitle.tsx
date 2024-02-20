af4f46ad110a9edb3bc7bbc87200b1c9e11df1c0
csyun <changsoonyun@naver.com>
2024-02-21 02:09:16 +0900

    #44 Style bg-color, mb, py추가

--- src/components/PageTitle.tsx
+++ src/components/PageTitle.tsx
@@ -1,13 +1,15 @@
 import { ReactNode } from 'react';
 
 interface PageTitleProps {
-  title: string;
+  title: string | ReactNode;
   children?: ReactNode;
 }
 export default function PageTitle({ title, children }: PageTitleProps) {
   return (
     <>
-      <div className={'relative flex items-center justify-center pb-16'} data-cy={'seller-title'}>
+      <div
+        className={'relative flex items-center justify-center bg-white mb-2 py-8'}
+        data-cy={'seller-title'}>
         <h2 className="flex-1 scroll-m-20 text-xl font-semibold tracking-tight first:mt-0 text-center">
           {title}
         </h2>
