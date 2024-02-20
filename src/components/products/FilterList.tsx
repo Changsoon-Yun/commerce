e80261d337aed71c3a65b5d8583e2ae1c37af8f8
csyun <changsoonyun@naver.com>
2024-02-21 02:09:42 +0900

    #44 Style 필터리스트 수정

--- src/components/products/FilterList.tsx
+++ src/components/products/FilterList.tsx
@@ -1,6 +1,7 @@
 import { RadioGroup, RadioGroupItem } from '@/components/ui/radio-group.tsx';
 import { Label } from '@/components/ui/label.tsx';
 import { filterArr } from '@/pages/products/CategoryProducts.page.tsx';
+import Container from '@/components/Container.tsx';
 
 interface FilterListProps {
   onChangeFilterHandler: (item: string) => void;
@@ -9,24 +10,26 @@ interface FilterListProps {
 export default function FilterList({ onChangeFilterHandler, selectedFilter }: FilterListProps) {
   return (
     <>
-      <div className={'flex justify-end pb-8'}>
-        <RadioGroup
-          defaultValue={filterArr[0]}
-          className={'flex gap-4'}
-          onValueChange={onChangeFilterHandler}>
-          {filterArr.map((item) => (
-            <div className="flex items-center space-x-2" key={item}>
-              <RadioGroupItem value={item} id={item} className={'hidden'} />
-              <Label
-                data-cy={`filter-button-${item}`}
-                htmlFor={item}
-                className={`cursor-pointer pb-1 ${selectedFilter === item ? 'font-bold border-b border-gray-600' : 'font-medium'}`}>
-                {item}
-              </Label>
-            </div>
-          ))}
-        </RadioGroup>
-      </div>
+      <Container>
+        <div className={'flex justify-center items-center py-4 mb-2'}>
+          <RadioGroup
+            defaultValue={filterArr[0]}
+            className={'flex gap-4'}
+            onValueChange={onChangeFilterHandler}>
+            {filterArr.map((item) => (
+              <div className="flex items-center space-x-2" key={item}>
+                <RadioGroupItem value={item} id={item} className={'hidden'} />
+                <Label
+                  data-cy={`filter-button-${item}`}
+                  htmlFor={item}
+                  className={`cursor-pointer pb-1 ${selectedFilter === item ? 'font-bold border-b border-gray-600' : 'font-medium'}`}>
+                  {item}
+                </Label>
+              </div>
+            ))}
+          </RadioGroup>
+        </div>
+      </Container>
     </>
   );
 }
