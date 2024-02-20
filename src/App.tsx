952ecc5d6c5b56b7006bda5336fc21021bc9c491
csyun <changsoonyun@naver.com>
2024-02-21 02:08:10 +0900

    #44 Fix 바뀐 Providers컴포넌트 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,55 +1,24 @@
 import Router from '@/lib/router/Router.tsx';
-import { MutationCache, QueryCache, QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import ScrollTop from '@/utils/ScrollTop.tsx';
-import { CartProvider } from '@/context/CartContext.tsx';
 import { Toaster } from './components/ui/toaster';
-import { FirebaseError } from 'firebase/app';
-import { Toast, toast, ToasterToast } from './components/ui/use-toast';
 import ErrorFallbackPage from '@/components/optimize/ErrorFallback.page.tsx';
 import { ErrorBoundary } from 'react-error-boundary';
 import SuspenseFallback from '@/components/optimize/SuspenseFallback.tsx';
 import { Suspense } from 'react';
-
-const queryErrorHandler = (
-  toast: ({ ...props }: Toast) => {
-    id: string;
-    dismiss: () => void;
-    update: (props: ToasterToast) => void;
-  },
-  e: unknown
-) => {
-  if (e instanceof FirebaseError) {
-    return toast({
-      title: '에러!',
-      description: e.code,
-      variant: 'destructive',
-    });
-  }
-};
-
-export const queryClient = new QueryClient({
-  queryCache: new QueryCache({
-    onError: (e) => queryErrorHandler(toast, e),
-  }),
-  mutationCache: new MutationCache({
-    onError: (e) => queryErrorHandler(toast, e),
-  }),
-});
+import Providers from './lib/router/Providers';
 
 export default function App() {
   return (
     <>
       <ErrorBoundary fallbackRender={ErrorFallbackPage}>
         <Suspense fallback={<SuspenseFallback />}>
-          <QueryClientProvider client={queryClient}>
+          <Providers>
             <ScrollTop />
-            <CartProvider>
-              <Router />
-              <Toaster />
-              <ReactQueryDevtools />
-            </CartProvider>
-          </QueryClientProvider>
+            <Router />
+            <Toaster />
+            <ReactQueryDevtools />
+          </Providers>
         </Suspense>
       </ErrorBoundary>
     </>
