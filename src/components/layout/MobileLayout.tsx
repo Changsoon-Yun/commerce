24e1c49579d9acac0ec476407fb9095011025557
csyun <changsoonyun@naver.com>
2024-02-22 06:53:03 +0900

    Style 레이아웃에 svh적용

--- src/components/layout/MobileLayout.tsx
+++ src/components/layout/MobileLayout.tsx
@@ -2,32 +2,29 @@ import { Outlet, useLocation } from 'react-router-dom';
 import Header from '@/components/header/Header.tsx';
 import Footer from '../footer/Footer.tsx';
 import SideCart from '../cart/SideCart.tsx';
-import { useEffect, useState } from 'react';
+import { useEffect } from 'react';
 
 export default function MobileLayout() {
   const { pathname } = useLocation();
-  const [innerHeight, setInnerHeight] = useState<number>(0);
+  // const [innerHeight, setInnerHeight] = useState<number>(0);
   useEffect(() => {
     document.getElementById('main')?.scrollTo(0, 0);
   }, [pathname]);
 
-  useEffect(() => {
-    if (typeof window !== 'undefined') {
-      setInnerHeight(window.innerHeight);
-    }
-  }, []);
+  // useEffect(() => {
+  //   if (typeof window !== 'undefined') {
+  //     setInnerHeight(window.innerHeight);
+  //   }
+  // }, []);
 
   return (
     <>
       <div className="bg-zinc-50 flex justify-center">
-        <div className="flex flex-1 flex-col relative max-w-lg w-full min-h-screen h-full shadow-lg  bg-zinc-100 overflow-hidden">
-          <header className={'sticky top-0 z-10 border-b border-zinc-200'}>
+        <div className="flex flex-1 flex-col relative max-w-lg w-full h-svh shadow-lg  bg-zinc-100 overflow-hidden">
+          <header className={'border-b border-zinc-200'}>
             <Header />
           </header>
-          <main
-            id={'main'}
-            style={{ maxHeight: innerHeight }}
-            className={'flex-1 flex flex-col min-h-0 basis-0 overflow-auto'}>
+          <main id={'main'} className={'flex-1 flex flex-col min-h-0 basis-0 overflow-auto'}>
             <Outlet />
           </main>
           <footer className={'border-t border-zinc-200 bg-zinc-50'}>
