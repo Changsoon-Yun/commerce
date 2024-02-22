dbfbfcf174db51eb4209b646e6d9aa92b2b2416a
csyun <changsoonyun@naver.com>
2024-02-22 21:56:44 +0900

    #50 Feat 테스트 라이브러리 추가 하면서 config 수정

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,16 @@
-/// <reference types="vitest" />
-//vite.config.ts
+import type { UserConfig } from 'vite';
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 import path from 'path';
 import { fileURLToPath } from 'url';
+import type { InlineConfig } from 'vitest';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
+
+interface VitestConfigExport extends UserConfig {
+  test: InlineConfig;
+}
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -16,4 +20,9 @@ export default defineConfig({
       '@': path.resolve(__dirname, 'src/'),
     },
   },
-});
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: ['./vitest.setup.ts'],
+  },
+} as VitestConfigExport);
