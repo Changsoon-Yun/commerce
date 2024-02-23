b31148842a6214c19892e7330a252cafd41bf076
csyun <changsoonyun@naver.com>
2024-02-23 21:03:50 +0900

    #55 Feat useInputChange.ts 커스텀 훅 추가

---
+++ src/hooks/useInputChange.ts
@@ -0,0 +1,21 @@
+import { ChangeEvent, useCallback, useState } from 'react';
+
+export default function useInputChange() {
+  const [isEditing, setIsEditing] = useState(false);
+  const [nickname, setNickname] = useState('John Doe'); // 닉네임 상태
+
+  const handleEditClick = useCallback(() => {
+    setIsEditing(true); // 수정 버튼 클릭 시 입력 모드로 변경
+  }, []);
+
+  const handleNicknameChange = useCallback((e: ChangeEvent<HTMLInputElement>) => {
+    setNickname(e.target.value); // 입력된 닉네임 업데이트
+  }, []);
+
+  const handleSaveClick = useCallback(() => {
+    setIsEditing(false); // 저장 버튼 클릭 시 입력 모드 종료
+    // 변경된 닉네임을 저장하거나 서버에 전송하는 등의 작업 수행
+  }, []);
+
+  return { isEditing, handleEditClick, handleSaveClick, handleNicknameChange, nickname };
+}
