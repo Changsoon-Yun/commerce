7dcfedbb2fb56b58ffa53a16e043c239502d410f
csyun <changsoonyun@naver.com>
2024-02-23 21:01:03 +0900

    #55 Style CardButton.tsx에 기본 스타일 추가

--- src/components/products/card/CardButton.tsx
+++ src/components/products/card/CardButton.tsx
@@ -1,14 +1,21 @@
 import { Button, ButtonProps } from '@/components/ui/button.tsx';
 import { ReactNode } from 'react';
+import { cn } from '@/lib/utils.ts';
 
 interface CardButtonProps extends ButtonProps {
   children: ReactNode;
+  className?: string;
 }
 
-export default function CardButton({ children, ...rest }: CardButtonProps) {
+export default function CardButton({ children, className, ...rest }: CardButtonProps) {
   return (
     <>
-      <Button className={'flex-1'} {...rest}>
+      <Button
+        className={cn(
+          'flex-1 inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 bg-primary text-primary-foreground hover:text-primary-foreground shadow hover:bg-primary/90 h-9 px-4 py-2',
+          className
+        )}
+        {...rest}>
         {children}
       </Button>
     </>
