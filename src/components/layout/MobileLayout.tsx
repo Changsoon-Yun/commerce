9a4674a63afc247e290a2d693a3032596986c953
csyun <changsoonyun@naver.com>
2024-02-23 21:03:10 +0900

    #55 Style 안쓰는 state 삭제

--- src/components/layout/MobileLayout.tsx
+++ src/components/layout/MobileLayout.tsx
@@ -2,31 +2,23 @@ import { Outlet, useLocation } from 'react-router-dom';
 import Header from '@/components/header/Header.tsx';
 import Footer from '../footer/Footer.tsx';
 import SideCart from '../cart/SideCart.tsx';
-import { useEffect, useState } from 'react';
+import { useEffect } from 'react';
 
 export default function MobileLayout() {
   const { pathname } = useLocation();
-  const [innerHeight, setInnerHeight] = useState<number>(0);
   useEffect(() => {
     document.getElementById('main')?.scrollTo(0, 0);
   }, [pathname]);
 
-  useEffect(() => {
-    if (typeof window !== 'undefined') {
-      setInnerHeight(window.innerHeight);
-    }
-  }, []);
-
   return (
     <>
       <div className="bg-zinc-50 flex justify-center">
-        <div className="flex flex-1 flex-col relative max-w-lg w-full min-h-screen h-full shadow-lg  bg-zinc-100 overflow-hidden">
+        <div className="flex flex-1 flex-col relative max-w-lg w-full min-h-screen h-full shadow-lg bg-zinc-100 overflow-hidden">
           <header className={'sticky top-0 z-10 border-b border-zinc-200'}>
             <Header />
           </header>
           <main
             id={'main'}
-            style={{ maxHeight: innerHeight }}
             className={'flex-1 flex flex-col min-h-0 basis-0 overflow-auto scrollbar-hide'}>
             <Outlet />
           </main>
