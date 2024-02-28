43018861422ada50183cdf182219c5694adffd56
csyun <changsoonyun@naver.com>
2024-02-29 07:44:20 +0900

    Feat Footer.tsx 로그인별 메뉴 UI 수정

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,12 +1,6 @@
 import { useAuth } from '@/apis/useAuth.ts';
 import { GoHome, GoHomeFill } from 'react-icons/go';
-import {
-  IoHeart,
-  IoHeartOutline,
-  IoLogInOutline,
-  IoPerson,
-  IoPersonOutline,
-} from 'react-icons/io5';
+import { IoHeart, IoHeartOutline, IoLogInOutline } from 'react-icons/io5';
 import { FaList } from 'react-icons/fa';
 import FooterIconWithCart from '@/components/footer/FooterIconWithCart.tsx';
 import FooterIconWithLabel from '@/components/footer/FooterIconWithLabel.tsx';
@@ -19,20 +13,16 @@ export default function Footer() {
       <div className={''}>
         <nav className={'flex justify-around'}>
           <FooterIconWithLabel to={'/'} icon={GoHome} exactIcon={GoHomeFill} text={'홈'} />
-          <FooterIconWithLabel
-            to={'/ordered-products'}
-            icon={FaList}
-            exactIcon={FaList}
-            text={'주문내역'}
-          />
-          <FooterIconWithCart icon={IoHeartOutline} exactIcon={IoHeart} text={'장바구니'} />
           {storedUserData ? (
-            <FooterIconWithLabel
-              to={'/user/dashboard'}
-              icon={IoPersonOutline}
-              exactIcon={IoPerson}
-              text={'마이페이지'}
-            />
+            <>
+              <FooterIconWithCart icon={IoHeartOutline} exactIcon={IoHeart} text={'장바구니'} />
+              <FooterIconWithLabel
+                to={'/ordered-products'}
+                icon={FaList}
+                exactIcon={FaList}
+                text={'주문내역'}
+              />
+            </>
           ) : (
             <FooterIconWithLabel
               icon={IoLogInOutline}
