d81b63d55de7d5cc869fd0ef6fc3fc8db1108dea
csyun <changsoonyun@naver.com>
2024-03-01 14:48:57 +0900

    #56 Feat 필요없어진 register select페이지 제거

--- src/pages/auth/Login.page.tsx
+++ src/pages/auth/Login.page.tsx
@@ -55,7 +55,7 @@ export default function LoginPage() {
             <p className={'text-center mt-8 text-sm text-zinc-600'}>
               계정이 없으신가요?
               <span className={'text-blue-500'}>
-                <Link to={'/register/select'}> 회원가입 하기</Link>
+                <Link to={'/register/seller'}> 회원가입 하기</Link>
               </span>
             </p>
             <SocialLogins
