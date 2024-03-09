5482bcb2a3e85ce1dd5408ee389e8cda77ce3f84
csyun <changsoonyun@naver.com>
2024-03-09 21:02:35 +0900

    #58 Feat useProfile.ts 기능 구현

--- src/apis/auth/useProfile.ts
+++ src/apis/auth/useProfile.ts
@@ -1,31 +1,79 @@
-import { ChangeEvent } from 'react';
-import { auth } from '@/lib/firebase/firebase.ts';
-import imageCompression from 'browser-image-compression';
-import { updateProfile } from 'firebase/auth';
+import { ChangeEvent, useEffect, useState } from 'react';
 import { handleFirebaseError } from '@/utils/handleFirebaseError.ts';
 import { toast } from '@/components/ui/use-toast.ts';
+import { useAuth } from '@/apis/auth/useAuth.ts';
+import { z } from 'zod';
+import { profileSchema } from '@/lib/zod/schemas.ts';
+import { getDownloadURL, ref, uploadBytes } from 'firebase/storage';
+import { db, storage } from '@/lib/firebase/firebase.ts';
+import { doc, updateDoc } from 'firebase/firestore';
+import { toastMessage } from '@/constant/toastMessage.ts';
+import { useNavigate } from 'react-router-dom';
+import { queryClient } from '@/lib/react-query/queryClient.ts';
+import { QUERY_KEYS } from '@/lib/react-query/queryKeys.ts';
 
 export default function useProfile() {
-  const updateProfileHandler = async (e: ChangeEvent<HTMLInputElement>) => {
-    try {
-      if (auth.currentUser && e.target.files) {
-        const file = e.target.files;
-        const compressedImage = await imageCompression(file[0], {
-          maxSizeMB: 1,
-          maxWidthOrHeight: 310,
-          useWebWorker: true,
-        });
-
-        const blobImage = await imageCompression.getDataUrlFromFile(compressedImage);
-        await updateProfile(auth.currentUser, {
-          photoURL: blobImage,
-        });
-      }
-    } catch (e) {
-      handleFirebaseError({ e, toast });
+  const navigate = useNavigate();
+  const { userData, storedUserData, fetchUserInfo } = useAuth();
+  const [profileImage, setProfileImage] = useState('');
+  const [uploadProfileImage, setUploadProfileImage] = useState<File>();
+  const [isLoading, setIsLoading] = useState(false);
+  const imageChangeHandler = (e: ChangeEvent<HTMLInputElement>) => {
+    if (e.target.files) {
+      const blobImage = URL.createObjectURL(e.target.files[0]);
+      setProfileImage(blobImage);
+      setUploadProfileImage(e.target.files[0]);
     }
   };
+
+  const updateProfileHandler = async (values: z.infer<typeof profileSchema>) => {
+    try {
+      if (!uploadProfileImage && !profileImage) {
+        alert('이미지를 등록해 주세요.');
+        return;
+      }
+      if (!userData) {
+        return;
+      }
+      const imageRef = ref(
+        storage,
+        userData.uid + '/' + 'profile' + ` ${uploadProfileImage?.name}`
+      );
+      await uploadBytes(imageRef, uploadProfileImage as File);
+      const updatedImageRef = ref(
+        storage,
+        userData.uid + '/' + 'profile' + ` ${uploadProfileImage?.name}`
+      );
+      const updatedImg = await getDownloadURL(updatedImageRef);
+      const userRef = doc(db, `users/${userData.uid}`);
+      await updateDoc(userRef, {
+        profileImg: uploadProfileImage ? updatedImg : profileImage,
+        userName: values.userName,
+      });
+      toast({ description: toastMessage.profile.description });
+      await queryClient.invalidateQueries({ queryKey: QUERY_KEYS.AUTH.USER() });
+      await fetchUserInfo();
+      navigate('/user/dashboard', { replace: true });
+    } catch (e) {
+      handleFirebaseError({ e, toast });
+      setIsLoading(false);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    if (storedUserData) {
+      setProfileImage(storedUserData.profileImg);
+    }
+  }, [storedUserData, userData]);
+
   return {
+    profileImage,
+    storedUserData,
     updateProfileHandler,
+    imageChangeHandler,
+    userData,
+    isLoading,
   };
 }
