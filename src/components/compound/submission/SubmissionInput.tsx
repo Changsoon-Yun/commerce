efc2c3274c9e4c0acd29d1092613ecf0542df261
csyun <changsoonyun@naver.com>
2024-03-11 02:53:52 +0900

    Fix SubmissionInput.tsx FormControl 컴포넌트 추가

--- src/components/compound/submission/SubmissionInput.tsx
+++ src/components/compound/submission/SubmissionInput.tsx
@@ -1,5 +1,6 @@
 import { Input } from '@/components/ui/input.tsx';
 import { SubmissionInputProps } from '@/types/submission.ts';
+import { FormControl, FormMessage } from '@/components/ui/form.tsx';
 
 export default function SubmissionInput<TName extends string>({
   field,
@@ -9,13 +10,16 @@ export default function SubmissionInput<TName extends string>({
 }: SubmissionInputProps<TName>) {
   return (
     <>
-      <Input
-        {...field}
-        data-testid={field.name}
-        type={type}
-        placeholder={placeholder}
-        className={`${hasPadding && 'pl-7'} [appearance:textfield] [&::-webkit-outer-spin-button]:appearance-none [&::-webkit-inner-spin-button]:appearance-none`}
-      />
+      <FormControl>
+        <Input
+          {...field}
+          data-testid={field.name}
+          type={type}
+          placeholder={placeholder}
+          className={`${hasPadding && 'pl-7'} [appearance:textfield] [&::-webkit-outer-spin-button]:appearance-none [&::-webkit-inner-spin-button]:appearance-none`}
+        />
+      </FormControl>
+      <FormMessage className={'pt-2'} data-testid={`${field.name}-error-msg`} />
     </>
   );
 }
